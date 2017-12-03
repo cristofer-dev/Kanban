dcc267d3d3d122069b1f2d6b0c2246c99a540eae
Cristofer <cristofer-dev@users.noreply.github.com>
2017-12-03 16:39:52 -0300

    ADD Line

--- src/components/ColumnBody/index.jsx
+++ src/components/ColumnBody/index.jsx
@@ -2,10 +2,6 @@ import React, { Component } from 'react';
 import styles from './column-body.css'
 
 class ColumnBody extends Component {
-	constructor (){
-		super()
-	}
-	
 	render() {
 		return (
 			<div className={`col-md-12 ${styles.columnBody} `}>      
