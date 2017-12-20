5688c53e252361179817cf2480d5ef356aa0a37a
Cristofer [HP] <cristofer.dev@gmail.com>
2017-12-20 02:19:03 -0300

    ADD Add tasks by props and addNew Task to state.
     Changes to be committed:
    	modified:   src/components/Column/index.jsx
    	modified:   src/components/NewTask/index.jsx
    
    TODO: Close textNewTask and clear value.

--- src/components/NewTask/index.jsx
+++ src/components/NewTask/index.jsx
@@ -5,16 +5,18 @@ class NewTask extends Component {
 
   render() {
     return (
-      <div className={`rounded row ${styles.newTask}`}>
-        <div className={`col-md-8`}>
-        <div class="input-group">
-          <input type="text" className={`form-control`} placeholder="New Task"/>
+      <form onSubmit={this.props.addNewTask}>
+        <div className={`rounded row ${styles.newTask}`}>
+          <div className={`col-md-8`}>
+          <div className="input-group">
+            <input type="text" name='text' className={`form-control`} placeholder="New Task"/>
+            </div>
+          </div>
+          <div className={`col-md-4`}>
+            <button type="submit" className="btn btn-sm btn-primary btn-block">Add</button>
           </div>
         </div>
-        <div className={`col-md-4`}>
-          <button type="button" class="btn btn-sm btn-primary btn-block">Add</button>
-        </div>
-      </div>
+      </form>
     );
   }
 }
