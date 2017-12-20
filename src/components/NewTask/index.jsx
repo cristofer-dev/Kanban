5dd6540a7bc6230b5e9ebfd9dc57f6a1a6364240
Cristofer [HP] <cristofer.dev@gmail.com>
2017-12-20 01:07:13 -0300

    ADD Btn add new task
     Changes to be committed:
    	modified:   src/components/NewTask/index.jsx
    TODO: onClick()

--- src/components/NewTask/index.jsx
+++ src/components/NewTask/index.jsx
@@ -5,8 +5,15 @@ class NewTask extends Component {
 
   render() {
     return (
-      <div className={`rounded ${styles.newTask}`}>
-        <input type="text"/>
+      <div className={`rounded row ${styles.newTask}`}>
+        <div className={`col-md-8`}>
+        <div class="input-group">
+          <input type="text" className={`form-control`} placeholder="New Task"/>
+          </div>
+        </div>
+        <div className={`col-md-4`}>
+          <button type="button" class="btn btn-sm btn-primary btn-block">Add</button>
+        </div>
       </div>
     );
   }
