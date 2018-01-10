d3c47504f50bf48a7488c05a406fc38aa319b6c3
Cristofer [HP] <cristofer.dev@gmail.com>
2018-01-09 22:36:05 -0300

    REFACTORING Component NewTask to StateLess
     Changes to be committed:
    	modified:   src/components/NewTask/index.jsx

--- src/components/NewTask/index.jsx
+++ src/components/NewTask/index.jsx
@@ -1,24 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
 import styles from './new-task.css'
 
-class NewTask extends Component {
 
-  render() {
-    return (
-      <form onSubmit={this.props.addNewTask}>
-        <div className={`rounded row ${styles.newTask}`}>
-          <div className={`col-md-8`}>
-          <div className="input-group">
-            <input type="text" name='text' className={`form-control`} placeholder="New Task"/>
-            </div>
-          </div>
-          <div className={`col-md-4`}>
-            <button type="submit" className="btn btn-sm btn-primary btn-block">Add</button>
+function NewTask(props) {
+  return (
+    <form onSubmit={props.addNewTask}>
+      <div className={`rounded row ${styles.newTask}`}>
+        <div className={`col-md-8`}>
+        <div className="input-group">
+          <input 
+            type="text" 
+            name='text' 
+            className={`form-control`} 
+            placeholder="New Task"
+          />
           </div>
         </div>
-      </form>
-    );
-  }
+        <div className={`col-md-4`}>
+          <button 
+            type="submit" 
+            className="btn btn-sm btn-primary btn-block">
+            Add
+          </button>
+        </div>
+      </div>
+    </form>
+  );
 }
 
 export default NewTask
