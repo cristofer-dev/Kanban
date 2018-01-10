cfaed4ce1257bf82781343d8bb1da6d5c7bd75dc
Cristofer [HP] <cristofer.dev@gmail.com>
2018-01-09 22:12:31 -0300

    REFACTORING Component ColumnHeader to StateLess
     Changes to be committed:
    	modified:   src/components/ColumnHeader/index.jsx

--- src/components/ColumnHeader/index.jsx
+++ src/components/ColumnHeader/index.jsx
@@ -1,26 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
 import styles from './column-header.css'
 
-class ColumnHeader extends Component {
-	constructor (){
-		super()
-	}
-	
-	render() {
-		return (
-			<div>
-			<div className={`col ${styles[this.props.action]} ${styles.columnHeader} ${styles.flexcontainer}`}>      
-			  <i className={`fa fa-${this.props.icon}`} aria-hidden="true"></i>
-			  {this.props.action}
-			</div>
-			<div className={`col`}>
-				
-				<i onClick={this.props.onOpenText} className={`rounded-circle fa fa-plus ${styles.plus}`} aria-hidden="true"></i>
-				
-			</div>
-			</div>
-		);
-	}
+function ColumnHeader(props) {
+  return (
+    <div>
+    <div className={`
+      col
+      ${styles[props.action]}
+      ${styles.columnHeader}
+      ${styles.flexcontainer}
+      `}>
+      <i className={`fa fa-${props.icon}`} aria-hidden="true"></i>
+      {props.action}
+    </div>
+    <div className={`col`}>
+      
+      <i onClick={props.onOpenText} 
+        className={`rounded-circle fa fa-plus ${styles.plus}`} 
+        aria-hidden="true">
+      </i>
+      
+    </div>
+    </div>
+  );
 }
 
 export default ColumnHeader
