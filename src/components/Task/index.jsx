d19fa1ee90405cf25546f50a4521315547ef5a34
Cristofer [HP] <cristofer.dev@gmail.com>
2018-01-10 00:27:07 -0300

    FIX "this." in component Task
     Changes to be committed:
    	modified:   src/components/Task/index.jsx

--- src/components/Task/index.jsx
+++ src/components/Task/index.jsx
@@ -4,7 +4,7 @@ import styles from './task.css'
 function Task(props) {
   return (
     <div className={`rounded ${styles.hvrFloatShadow} ${styles.task} `}>
-      {this.props.text}
+      {props.text}
       <img 
         src="https://randomuser.me/api/portraits/women/72.jpg" 
         className="rounded-circle float-right"
