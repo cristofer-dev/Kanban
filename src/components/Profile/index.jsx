3ef012c0ad8ef8b0021a3d0a66b024ac9aa90598
Cristofer [HP] <cristofer.dev@gmail.com>
2018-01-25 23:25:44 -0300

    ADD Profile component
     Changes to be committed:
    	new file:   src/components/Profile/index.jsx
    	new file:   src/components/Profile/profile.css

---
+++ src/components/Profile/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './profile.css'
+
+function Profile() {
+  return (
+    <div className={`col-md-12 ${styles.header} `}>
+      Profile
+    </div>
+  );
+}
+
+export default Profile
