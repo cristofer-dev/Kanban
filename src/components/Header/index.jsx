6f3a31c1eff1b6123894f59b80e9405f594d119c
Cristofer [HP] <cristofer.dev@gmail.com>
2018-01-27 21:39:54 -0300

    ADD Link example to profile
     Changes to be committed:
    	modified:   src/components/Header/index.jsx

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 import styles from './header.css'
 
 function Header(props) {
   return (
     <div className={`col-md-12 ${styles.header} `}>
       Mis Tareas
+      <div>
+        <Link to="/profile">View Profile</Link>
+      </div>
     </div>
+    
   );
 }
 
